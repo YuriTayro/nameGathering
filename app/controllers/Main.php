10f2786b927bb0c72712a913773ed9d27e3504c7
yuri.tayrocnp <yuri.tayrocnp@gmail.com>
2025-08-25 21:37:38 -0300

    implements database files

--- app/controllers/Main.php
+++ app/controllers/Main.php
@@ -2,8 +2,15 @@
 
 namespace bng\Controllers; //nome ficticio q eu atribui, para ser usado no composer.json 
 use bng\Controllers\BaseController;
+use bng\Models\Agents;
 class Main extends BaseController {
     public function index(){
+
+        $models = new Agents;
+        $results = $models->get_total_Agents();
+        printData($results);
+
+
         $data['nome'] = 'Bruno';
         $data['apelido'] = 'Ribeiro';
         $this->view('layouts/html_header');
